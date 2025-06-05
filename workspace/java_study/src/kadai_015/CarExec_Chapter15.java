96bc251caca5492fd39b10cda2703814fbc43a17
samurai-hanahana <mizukoro25@yahoo.co.jp>
2025-06-06 00:33:45 +0900

    課題提出

---
+++ workspace/java_study/src/kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		Car_Chapter15 car01 = new Car_Chapter15();
+		car01.gearChange(3);
+		car01.run();
+	}
+}
