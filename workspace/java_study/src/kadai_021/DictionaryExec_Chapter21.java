a1d8da1e9ca1ac1e55575b750d29f596ebe54ef4
samurai-hanahana <mizukoro25@yahoo.co.jp>
2025-06-14 03:35:45 +0900

    Java辞書課題提出

---
+++ workspace/java_study/src/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,9 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		String[] checkWords = { "apple", "banana", "grape", "orange" };
+		dictionary.checkWords(checkWords);
+	}
+}
